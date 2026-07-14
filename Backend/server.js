c02685aa30a245705dbe804b692d7a6e006155fd
sanku5003 <pariharsanket10@gmail.com>
2026-07-14 15:47:39 +0530

    some minor changes

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,8 @@ const connectDB = require("./src/config/database");
 
 connectDB();
 
-app.listen(4000, () => {
-  console.log("Server is running on port 4000");
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
