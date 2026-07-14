294580a75834667ab65972d89b24ad72e93161a6
sanku5003 <pariharsanket10@gmail.com>
2026-07-14 17:31:30 +0530

    some changes

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -124,7 +124,6 @@ ${jobDescription}
 async function generatePdfFromHtml(htmlContent) {
   const browser = await puppeteer.launch({
   headless: true,
-  executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
   args: [
     "--no-sandbox",
     "--disable-setuid-sandbox",
