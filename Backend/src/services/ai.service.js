5227ebc58f23deeb8ef5efbf2239dcda31b6341a
sanku5003 <pariharsanket10@gmail.com>
2026-07-14 19:08:48 +0530

    some changes

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,8 +1,8 @@
 const { GoogleGenAI } = require("@google/genai");
 const { z } = require("zod");
 const { zodToJsonSchema } = require("zod-to-json-schema");
-const puppeteer = require("puppeteer-core");
-const chromium = require("@sparticuz/chromium");
+const puppeteer = require("puppeteer");
+
 const ai = new GoogleGenAI({
   apiKey: process.env.GOOGLE_GENAI_API_KEY,
 });
